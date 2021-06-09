e46f4a916c09b85f6cfef0b7325a31e73151a65c
Robert Garcia <rgs2@icomp.ufam.edu.br>
2021-06-09 17:17:06 -0400

    chore: Create users

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,4 +2,27 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
+const user = {
+  id: '',
+  name: '',
+  username: '',
+  todos: []
+}; 
+
+
+app.post('/users', (request, response) => {
+  const { id, name, username} = request.body;
+
+  user.id = id;
+  user.name = name;
+  user.username = username;
+
+  return response.json({
+    user: user
+  })
+})
+
+
 app.listen(3333);
